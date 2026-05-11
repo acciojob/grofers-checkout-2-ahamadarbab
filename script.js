02524fc22417ae7cfb5ff65b9a8541da36030340
Arbab Ahamad <ahmadarbab.456@gmail.com>
2026-05-11 22:59:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your code here
+
+// select all price cells
+let prices = document.querySelectorAll('[data-ns-test="prices"]');
+
+let total = 0;
+
+// calculate total
+prices.forEach((price) => {
+	total += Number(price.textContent);
+});
+
+// Create new row
+let totalRow = document.createElement("tr");
+
+// Create new cell
+let totalCell = document.createElement("td");
+
+// set attributes and content
+totalCell.setAttribute("data-ns-test", "grandTotal");
+totalCell.colSpan = 2;
+totalCell.textContent = total;
+
+// append cell to row
+totalRow.appendChild(totalCell);
+
+// append row to table
+document.getElementById("groceryTable").appendChild(totalRow);
